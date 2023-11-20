e0d06f1fdb5c34e38df0d3399f0f6be12a4c5570
202311130516 <150647208+202311130516@users.noreply.github.com>
2023-11-20 04:12:07 +0000

    Update Manager.java

--- Manager.java
+++ Manager.java
@@ -1,6 +1,6 @@
 public class Manager extends Admin {
-  public Manager (String username, String password, Admin admin) {
-    if () {return };
+  public Manager (String username, String password, Admin admin) throws Exception {
+    if (!admin.getStatus()) {throw new Exception("Admin not logged in")};
     this.username = username;
     this.salt = (int)(Math.random()*0xffff);
     this.passwordHash = hashString(password);
