3a17ed29138e77b08212845ac338ac81b9482601
202311130516 <150647208+202311130516@users.noreply.github.com>
2023-11-20 04:11:07 +0000

    Update Manager.java

--- Manager.java
+++ Manager.java
@@ -1,3 +1,8 @@
 public class Manager extends Admin {
-  
+  public Manager (String username, String password, Admin admin) {
+    if () {return };
+    this.username = username;
+    this.salt = (int)(Math.random()*0xffff);
+    this.passwordHash = hashString(password);
+  }
 }
