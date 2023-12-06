dfeb64aebee76b365bc83749c94cf9cb659b5cd5
Oaroki <121933094+oaroki-git@users.noreply.github.com>
2023-12-06 21:13:52 +0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -9,6 +9,8 @@ public class Main {
     static int choice = 0;
     static int page = 'l';
     static Account account = null;
+    static String str = "";
+    static String[] split = null;
 
     //mainloop
     public static void main(String[] args) throws Exception{
@@ -18,15 +20,14 @@ public class Main {
 	while (proceed) {
 	    switch(page){
 		case 'l': loginPage(); break;
-		case 'm': managerPage(); break;
-		case 'u': userPage(); break;
+		case 'm': managerPage(account); break;
+		case 'u': userPage(account); break;
 		default: IO.print("page not found");
 	    }
     	}
     }
 
     //create accounts
-
     public static void create(String username, String password) throws Exception{
     	accounts.add(new Admin(username, password));}
 
@@ -37,12 +38,12 @@ public class Main {
     	accounts.add(new User(username, password, address, admin));}
 
    //login to account
-
     public static Account get(String username)throws Exception{return accounts.get(username);} //get account and pass to login
     public static void login(Account account, String password)throws Exception{account.login(password);}
 
     //interfaces
 
+    //login
     public static void loginPage()throws Exception{
 	try{choice = Integer.parseInt(IO.input("select login \n1. manager   2. user\n"));} 
 	catch (NumberFormatException e){
@@ -52,57 +53,129 @@ public class Main {
 	}
 
 	if ( choiceCheck(2)){
-	    String str = IO.input("enter username and password (separated by commas)\n");
-	    String[] split = str.split(",");
+	    str = IO.input("enter username and password (separated by commas)\n");
+	    split = str.split(",");
 	    account = get(split[0]);
 	    login(account, split[1]);
 
 	    switch(choice){
 		case 1: page = 'm'; return;
 		case 2: page = 'u'; return;
-	    //change after adding the getIdentity funtions
+	    //change after adding the getIdentity funtions(or not)
 	    }
 	}
     }
 
+    //manager pages
     public static void managerPage(Account account)throws Exception{
-	IO.input("~manager~\n");
-    }
+	if(account == null){IO.print("error no user passed\n"); return;}
+	if(!(account instanceof Admin)) {IO.print("error account type mismatch\n"); return;}
 
-    public static void userPage(Account account)throws Exception{
-	try{choice = Integer.parseInt(IO.input("~user~\n1. deposit 2. withdraw 3. settings\npress any key to log out\n"));}
+	Admin manager = (Admin) account;
+	IO.print("logged in as manager" + manager.getUsername() + "\n");
+	
+	try{choice = Integer.parseInt(IO.input("~manager~\n1. view transactions 2. add account 3. change interest rate \npress any key to log out\n"));}
 	catch (NumberFormatException e){
-	    IO.print("invalid choice\n")
-	    page = 'l'
+	    manager.logout();
+	    page = 'l';
 	    return;
 	}
+
+	switch(choice){
+	    case 1: return;
+	    case 2: addAccountPage(manager); return;
+	    case 3: changeInterestPage(manager); return;
+	    //case [n]: managerSettingsPage(manager); //for if we need a settings page
+	    default: manager.logout(); page = 'l'; return;
+	}
+	
+    }
+
+    public static void changeInterestPage(Admin manager) throws Exception{
+	if(manager == null){IO.print("error no user passed\n"); return;}
+	IO.print("this functionality does not exist yet\n");
+    };
+
+    public static void addAccountPage(Admin manager) throws Exception{
+	if(manager == null){IO.print("error no user passed\n"); return;}
+
+	try{choice = Integer.parseInt(IO.input("~add account~\n1. user 2. manager \npress any key to go back\n"));}
+	catch (NumberFormatException e){return;}
 
 	switch(choice){
 	    case 1:
+		str = IO.input("enter username, password and adress of new user (separated by commas)\n");
+		split = str.split(",");
+		create(split[0], split[1], manager, split[2]);
+		IO.print("user " + split[0] + " created");
+		return;
 	    case 2:
-	    case 3: page = 's';
-	    default: page = 'l'; return;
+	        str = IO.input("enter username and password of new manager (separated by commas)\n");
+    		split = str.split(",");
+		create(split[0], split[1], manager);
+		IO.print("manager " + split[0] + " created");
+		return;
+	    default: return;
 	}
     }
+    
+    //manager doesn't need a settings page (i thought it did and idk if we'll add one so here is the leftover code)
+    /*
+    public static void managerSettingsPage(Admin manager){
+	if(manager == null){IO.print("error no user passed\n"); return;}
 
-    public static void userSettingsPage(Account account)throws Exception{
-	try{choice = Integer.parseInt(IO.input("~user settings~\n1. change password 2. change address \n press any key to go back\n"));}
+	try{choice = Integer.parseInt(IO.input("~manager settings~\n1. change password 2. change address \n press any key to go back\n"));}
 	catch (NumberFormatException e){
-	    IO.print("invalid choice\n")
-	    page = 's'
+	    IO.print("invalid choice\n");
+	    page = 's';
+	    return;
+	}
+
+    }
+    */
+
+    //user pages
+    public static void userPage(Account account)throws Exception{
+	if(account == null){IO.print("error no user passed\n"); return;}
+	if(!(account instanceof User)) {IO.print("error account type mismatch\n"); return;}
+	
+	User user = (User) account;
+	IO.print("logged in as user " + user.getUsername() + "\n");
+
+	try{choice = Integer.parseInt(IO.input("~user~\n1. deposit 2. withdraw 3. settings\npress any key to log out\n"));}
+	catch (NumberFormatException e){
+	    user.logout();
+	    page = 'l';
 	    return;
 	}
 
 	switch(choice){
+	    case 1: return;
+	    case 2: return;
+	    case 3: userSettingsPage(user); return;
+	    default: user.logout(); page = 'l'; return;
+	}
+    }
+
+    public static void userSettingsPage(User user)throws Exception{
+	if(user == null){IO.print("error no user passed\n"); return;}
+
+	try{choice = Integer.parseInt(IO.input("~user settings~\n1. change password 2. change address \n press any key to go back\n"));}
+	catch (NumberFormatException e){return;}
+
+	switch(choice){
 	    case 1: 
-		account.setPassword(IO.input("enter your old password"), IO.input("enter your new password"));
+		user.setPassword(IO.input("enter your old password"), IO.input("enter your new password"));
 	    case 2:
-		account.setAddress(IO.input("enter your password again"), IO.input("enter your new address"));
-	    default: page = 'l'; return;
+		user.setAddress(IO.input("enter your password again"), IO.input("enter your new address"));
+	    default: return;
 
 	}
     }
 
     //quality of life
+    //this pretty much became useless anyway but i'm too lazy to delete
+    //maybe someone else do it for me thx <3
+    //it's literally only used once
     public static boolean choiceCheck(int choices){return (choice < choices+1 && choice > 0);}
-
+}
